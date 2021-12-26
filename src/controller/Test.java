674abd9a58931a775f8d04b3e11eb00c7b67802a
Kawther Gacem <kaoutheralbertinagacem@gmail.com>
2021-12-26 11:54:18 +0100

    test

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -8,26 +8,32 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import model.Question;
+import utils.DBConnection;
 
+import javax.swing.*;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import static java.awt.Color.RED;
 import static utils.DBConnection.getConnection;
 
 public class Test implements Initializable {
 
     @FXML
     private JFXButton addBtn;
-
-    @FXML
-    private JFXMasonryPane masonaryPane;
 
     @FXML
     private TextArea textArea;
@@ -38,44 +44,58 @@ public class Test implements Initializable {
     public void initialize (URL location, ResourceBundle resources){
 
 
+        String sqlQuery = "SELECT * FROM question";
 
+        try (Connection c = DBConnection.getConnection()){
+            Statement st = c.createStatement();
+            ResultSet rs = st.executeQuery(sqlQuery);
+            Question question;
 
-
-
-
-//        String sqlQuery = "SELECT * FROM products";
-
-//        try (Connection c = DBUtils.getConnection()){
-//            Statement st = c.createStatement();
-//            ResultSet rs = st.executeQuery(sqlQuery);
-//            Product product;
-//
-//            while(rs.next()){
-//                product = new Product(rs.getInt("product_id"),
-//                        rs.getString("product_name"),
-//                        rs.getDouble("purchased_price"),
-//                        rs.getDouble("sold_price"),
-//                        rs.getString("expiration_date"),
-//                        rs.getString("category"),
-//                        rs.getInt("quantity"),
-//                        rs.getInt("quantity") * rs.getDouble("purchased_price"));
-//                list.add(product);
-//            }
-//        }catch (Exception ex){
-//            ex.printStackTrace();
-//        }
+            while(rs.next()){
+                question = new Question(
+                        rs.getString("titre"),
+                        rs.getInt("up"),
+                        rs.getInt("down"));
+                createHbox(question);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
 
 
 
     }
+    public void createHbox(Question question){
+        Label titleLabel = new Label(question.getTitre());
+            titleLabel.setMaxHeight(50);
+            titleLabel.setMaxWidth(260);
+            titleLabel.setFont(new Font("System",20));
 
+        Label upLabel = new Label(String.valueOf(question.getUp()));
+            upLabel.setPrefSize(50,70);
+            upLabel.setAlignment(Pos.CENTER);
+            upLabel.setBackground(new Background(new BackgroundFill(Color.RED,CornerRadii.EMPTY, Insets.EMPTY)));
+            upLabel.setFont(new Font("System",20));
+
+        Label downLabel = new Label(String.valueOf(question.getDown()));
+            downLabel.setPrefSize(50,70);
+            downLabel.setAlignment(Pos.CENTER);
+            downLabel.setBackground(new Background(new BackgroundFill(Color.RED,CornerRadii.EMPTY, Insets.EMPTY)));
+            downLabel.setFont(new Font("System",20));
+
+
+        HBox hbox = new HBox();
+            hbox.setMaxHeight(50); hbox.setMaxWidth(400);
+            hbox.getChildren().addAll(titleLabel,upLabel,downLabel);
+            HBox.setHgrow(titleLabel, Priority.ALWAYS);
+
+
+            listView.getItems().add(hbox);
+
+    }
+
+    @FXML
     void addBtnOnClick(ActionEvent event) {
-        Pane pane = new Pane();
-        Label textLabel = new Label();
-        textLabel.setText(textArea.getText());
-        textLabel.setLayoutX(11);
-        pane.getChildren().add(textLabel);
-        masonaryPane.getChildren().add(pane);
+
     }
-    Connection connection = getConnection();
 }
---
+++ src/model/Question.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Question {
+    int id;
+    String titre;
+    int up;
+    int down;
+
+    public Question( String titre, int up, int down) {
+        this.titre = titre;
+        this.up = up;
+        this.down = down;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+
+    public int getUp() {
+        return up;
+    }
+
+    public void setUp(int up) {
+        this.up = up;
+    }
+
+    public int getDown() {
+        return down;
+    }
+
+    public void setDown(int down) {
+        this.down = down;
+    }
+}
