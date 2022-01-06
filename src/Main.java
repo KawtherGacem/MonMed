04843ffaefaeaf1123a7683e0eb1e30b7fafa046
torki meriem amira <ma.torki@esi-sba.dz>
2022-01-06 14:03:01 +0100

    4

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,26 @@
-
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/Test.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/Acceuilmed.fxml"));
         primaryStage.setTitle("Test");
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("view/Css/Css.css")).toExternalForm());
     }
 
 
+
+ 
     public static void main(String[] args) {
         launch(args);
     }
